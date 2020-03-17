6f6bd2d88f419eb406c806ae1631dbe56d684d0f
doatridge <darryl@opengrass.io>
2020-03-17 11:30:00 -0500

    - update to the latest release of AI-STAC

--- ds_skeleton/intent/skeleton_intent_model.py
+++ ds_skeleton/intent/skeleton_intent_model.py
@@ -2,7 +2,9 @@ import inspect
 import threading
 from copy import deepcopy
 
-from aistac.properties.abstract_properties import AbstractPropertyManager
+from aistac.components.aistac_commons import AistacCommons
+
+from ds_skeleton.managers.skeleton_property_manager import SkeletonPropertyManager
 from aistac.intent.abstract_intent import AbstractIntentModel
 
 __author__ = ''
@@ -10,27 +12,26 @@ __author__ = ''
 
 class SkeletonIntentModel(AbstractIntentModel):
 
-    def __init__(self, property_manager: AbstractPropertyManager, default_save_intent: bool = True,
-                 default_intent_level: [int, float, str] = None, default_replace_intent: bool = None,
-                 intent_type_additions: list = None):
-        """initialisation of the Intent class. The 'intent_param_exclude' is used to exclude commonly used method
-         parameters from being included in the intent contract, this is particularly useful if passing a canonical, or
-         non relevant parameters to an intent method pattern. Any named parameter in the intent_param_exclude list
-         will not be included in the recorded intent contract for that method
+    def __init__(self, property_manager: SkeletonPropertyManager, default_save_intent: bool=True,
+                 default_intent_level: bool=None, order_next_available: bool=None, default_replace_intent: bool=None):
+        """initialisation of the Intent class.
 
         :param property_manager: the property manager class that references the intent contract.
         :param default_save_intent: (optional) The default action for saving intent in the property manager
-        :param intent_next_available: (optional) if the default level should be set to next available level or zero
+        :param default_intent_level: (optional) the default level intent should be saved at
+        :param order_next_available: (optional) if the default behaviour for the order should be next available order
+        :param default_replace_intent: (optional) the default replace existing intent behaviour
         """
-        # set all the defaults
         default_save_intent = default_save_intent if isinstance(default_save_intent, bool) else True
         default_replace_intent = default_replace_intent if isinstance(default_replace_intent, bool) else True
-        default_intent_level = default_intent_level if isinstance(default_intent_level, (int, float, str)) else -1
-        intent_param_exclude = ['inplace', 'canonical']
-        intent_type_additions = []
-        super().__init__(property_manager=property_manager, intent_param_exclude=intent_param_exclude,
-                         default_save_intent=default_save_intent, default_intent_level=default_intent_level,
-                         default_replace_intent=default_replace_intent, intent_type_additions=intent_type_additions)
+        default_intent_level = default_intent_level if isinstance(default_intent_level, (str, int, float)) else 'A'
+        default_intent_order = -1 if isinstance(order_next_available, bool) and order_next_available else 0
+        intent_param_exclude = ['df', 'inplace', 'canonical', 'feature']
+        intent_type_additions = [np.int8, np.int16, np.int32, np.int64, np.float16, np.float32, np.float64]
+        super().__init__(property_manager=property_manager, default_save_intent=default_save_intent,
+                         intent_param_exclude=intent_param_exclude, default_intent_level=default_intent_level,
+                         default_intent_order=default_intent_order, default_replace_intent=default_replace_intent,
+                         intent_type_additions=intent_type_additions)
 
     def run_intent_pipeline(self, canonical, intent_levels: [int, str, list] = None, **kwargs):
         """ Collectively runs all parameterised intent taken from the property manager against the code base as
@@ -48,36 +49,48 @@ class SkeletonIntentModel(AbstractIntentModel):
         if self._pm.has_intent():
             # get the list of levels to run
             if isinstance(intent_levels, (int, str, list)):
-                intent_levels = self._pm.list_formatter(intent_levels)
+                intent_levels = AistacCommons.list_formatter(intent_levels)
             else:
                 intent_levels = sorted(self._pm.get_intent().keys())
             for level in intent_levels:
-                for method, params in self._pm.get_intent(level=level).items():
-                    if method in self.__dir__():
-                        params.update(params.pop('kwargs', {}))
-                        if isinstance(kwargs, dict):
-                            params.update(kwargs)
-                        params.update({'inplace': False, 'save_intent': False})
-                        canonical = eval(f"self.{method}(canonical, **{params})", globals(), locals())
-            return canonical
+                level_key = self._pm.join(self._pm.KEY.intent_key, level)
+                for order in sorted(self._pm.get(level_key, {})):
+                    for method, params in self._pm.get(self._pm.join(level_key, order), {}).items():
+                        if method in self.__dir__():
+                            # fail safe in case kwargs was sored as the reference
+                            params.update(params.pop('kwargs', {}))
+                            # add method kwargs to the params
+                            if isinstance(kwargs, dict):
+                                params.update(kwargs)
+                            # add excluded params and set to False
+                            params.update({'inplace': False, 'save_intent': False})
+                            canonical = eval(f"self.{method}(canonical, **{params})", globals(), locals())
+        return canonical
 
     def intent_method(self, canonical, inplace: bool=False,
-                      save_intent: bool=None, replace_intent: bool=None, intent_level: [int, str]=None):
+                      save_intent: bool = None, intent_level: [int, str] = None, intent_order: int = None,
+                      replace_intent: bool = None, remove_duplicates: bool = None):
         """ clean the headers of a pandas DataFrame replacing space with underscore
 
         :param canonical: the canonical to act this intent on
         :param inplace: if the canonical should be aceted upon in place or a copy returned
         :param replace_intent: if the intent from other levels should be replaced, irrelevant of parameter difference
         :param save_intent: (optional) if the intent contract should be saved to the property manager
-        :param intent_level: (optional) the level of the intent,
-                        If None: default's 0 unless the global intent_next_available is true then -1
+        :param intent_level: (optional) the level name that groups intent by a reference name
+        :param intent_order: (optional) the order in which each intent should run.
+                        If None: default's to -1
                         if -1: added to a level above any current instance of the intent section, level 0 if not found
                         if int: added to the level specified, overwriting any that already exist
+        :param replace_intent: (optional) if the intent method exists at the level, or default level
+                        True - replaces the current intent method with the new
+                        False - leaves it untouched, disregarding the new intent
+        :param remove_duplicates: (optional) removes any duplicate intent in any level that is identical
         :return: if inplace, returns a formatted cleaner contract for this method, else a deep copy pandas.DataFrame.
         """
         # resolve intent persist options
         self._set_intend_signature(self._intent_builder(method=inspect.currentframe().f_code.co_name, params=locals()),
-                                   intent_level=intent_level, save_intent=save_intent, replace_intent=replace_intent)
+                                   intent_level=intent_level, intent_order=intent_order, replace_intent=replace_intent,
+                                   remove_duplicates=remove_duplicates, save_intent=save_intent)
         # intend code block on the canonical
 
         if not inplace:
